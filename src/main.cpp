ca99a65960f258e5aa012491dc3e630ff3711b84
nmhlog <naufal.muhammad01@ui.ac.id>
2021-01-11 00:24:21 +0700

    Adding comments untuk beberapa hal 75%

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-// Deklarasi Variable 
+// Deklarasi Variable pin constant
 const int RELAY1_LAMP_PIN = 30;
 const int RELAY2_FAN_PIN = 31;
 const int LED_PIN = 32;
@@ -16,25 +16,40 @@ const int DHT11_PIN =7;
 bool buffer_state = true;
 dht DHT;
 //Deklarasi Task
-void Task_Read_SerialData( void *pvParameters );
-void Task_Ultrasound( void *pvParameters );
-void Task_Ultrasound_nopeople( void *pvParameters );
+void Task_read_sensors( void *pvParameters ); // Task untuk membaca sensor DHT11,LDR
+void Task_Ultrasound( void *pvParameters ); // Task untuk HCSR04, untuk mendeteksi keadaan sekitar saat terdeteksi manusia
+void Task_Ultrasound_nopeople( void *pvParameters ); // Task untuk HC-SRO4 untuk mendektesi manusia saat tidak ada orang
 
-int sensor_ultrasound_HCSR04(int trigpin, int echopin)
+int sensor_ultrasound_HCSR04(int trigpin, int echopin, bool read_data =true)
 {
+/* 
+fungsi untuk membaca HC-SR04 sensor 
+  param trigpin : Pin trigger
+  param echopin : Pin echo
+  return integer dari jarak pengukuran
+ */
   digitalWrite(trigpin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigpin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigpin, LOW);
-  int range = pulseIn(echopin, HIGH)/ 29 / 2; 
-  Serial.print("Distance from ultraound HCRS04 : ");
-  Serial.print(range);
-  Serial.println(" cm");
+  int range = pulseIn(echopin, HIGH)/ 29 / 2;  // Membaca jarak
+  if(read_data)
+  {  
+    Serial.print("Distance from ultraound HCRS04 : ");
+    Serial.print(range);
+    Serial.println(" cm");
+  }
+  
   return range;
   }
 
 bool get_person_state(int param){
+/* 
+Fungsi untuk mendapatkan keadaan manusi sekarang 
+param param : set jarak yang ditentukan untuk mengindikasikan seseorang sedang berkerja
+return : boolean keadaan manusia 1 indikasi ada manusia dan 0 indikasi tidak ada manusia
+ */
   bool flag =0;
   int buffer_range = sensor_ultrasound_HCSR04(TRIG_PIN,ECHO_PIN);
   if (buffer_range<param){
@@ -47,9 +62,17 @@ bool get_person_state(int param){
 
 
 
-void set_control(bool lamp_state,bool pin_state, bool led_state ){
+void set_control(bool lamp_state,bool fan_state, bool led_state ){
+/* 
+  fungsi untuk mengerakkan aktuator.
+  param  lamp_state : keadaan lampu 0 hidup || 1 mati
+  param  fan_state  : keadaan kipas 0 hidup || 1 mati
+  param  led_state  : keadaan kipas 1 hidup || 0 mati
+
+ */
+
   digitalWrite(RELAY1_LAMP_PIN, lamp_state);
-  digitalWrite(RELAY2_FAN_PIN, pin_state);
+  digitalWrite(RELAY2_FAN_PIN, fan_state);
   digitalWrite(LED_PIN, led_state);
   }
 
@@ -57,16 +80,16 @@ void setup()
 {   
       pinMode(TRIG_PIN, OUTPUT); // Sets the trigPin as an OUTPUT
       pinMode(ECHO_PIN, INPUT); // Sets the echoPin as an INPUT
-      pinMode(RELAY1_LAMP_PIN, OUTPUT); // Sets the RELAY1_LAMP as an INPUT
-      pinMode(RELAY2_FAN_PIN, OUTPUT); // Sets the RELAY2_FAN as an INPUT
-      pinMode(LED_PIN, OUTPUT); // Sets the echoPin as an INPUT
+      pinMode(RELAY1_LAMP_PIN, OUTPUT); // Sets the RELAY1_LAMP_PIN as an OUTPUT
+      pinMode(RELAY2_FAN_PIN, OUTPUT); // Sets the RELAY2_FAN_PIN as an OUTPUT
+      pinMode(LED_PIN, OUTPUT); // Sets the LED_PIN as an OUPUT
       xTaskCreate(
-        Task_Read_SerialData
+        Task_read_sensors
         , "Blink" 
         , 128      
         , NULL
         , 1        
-        , NULL );
+        , NULL ); // Fungsi untuk membaca task
  
     xTaskCreate(
         Task_Ultrasound
@@ -74,7 +97,7 @@ void setup()
         , 128    
         , NULL
         , 2       
-        , NULL );
+        , NULL ); // Fungsi untuk membaca task
 
 }
  
@@ -84,23 +107,25 @@ void loop()
 }
 
  
-void Task_Read_SerialData(void *pvParameters) // This is a task.
+void Task_read_sensors(void *pvParameters) // Task untuk membaca parameter dari sensor
 {   
     (void) pvParameters;
     int sensorValue ;
     Serial.begin(9600);
-    for (;;) // A Task shall never return or exit.
+    for (;;) 
     {
+      // Start Reading sensor
     int chk = DHT.read11(DHT11_PIN);
     sensorValue = analogRead(A0); 
     delay(60);
+    // Mengirim hasil pengukuran 
     Serial.println("Lampu = "+ String(sensorValue));
     Serial.print("Temperature = ");
     Serial.println(DHT.temperature);
     Serial.print("Humidity = ");
     Serial.println(DHT.humidity);   
-    vTaskDelay( 20000 / portTICK_PERIOD_MS );
-    } //20000 -> 20 SECOND
+    vTaskDelay( 20000 / portTICK_PERIOD_MS ); // delay task untuk 20000 = 20s
+    } 
 }
  
 void Task_Ultrasound(void *pvParameters) // This is a task.
@@ -112,19 +137,17 @@ void Task_Ultrasound(void *pvParameters) // This is a task.
     { 
       bool start = get_person_state(param);
       if (!start || buffer_state!=start){
-        for (int i= 0;i<5;i++)
-        {
-        start= start || get_person_state(param);
-        delay(10);
-        if(start){break;} 
+        for (int i= 0;i<5;i++){
+          start= start || get_person_state(param);
+          delay(10);
+          if(start){break;} 
         }
         
-      if (start != buffer_state)
-    {
-    buffer_state = start;
-    set_control(!start,!start,!start);
-    }
-        if(!start){
+      if (start != buffer_state){
+          buffer_state = start;
+          set_control(!start,!start,!start);
+          }
+      if(!start){
           xTaskCreate(Task_Ultrasound_nopeople, "Detecting no People", 128, NULL, 2, NULL );
           vTaskDelete(NULL);
         }
@@ -150,6 +173,7 @@ void Task_Ultrasound_nopeople( void *pvParameters ){
         vTaskDelete(NULL);
           }
         }
+        Serial.println("Person isn't Detected");
         // Serial.write()
         vTaskDelay( 20000 / portTICK_PERIOD_MS );
         
